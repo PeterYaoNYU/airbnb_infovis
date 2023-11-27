6b08fc98eda34227b3cced9969011a1d2480aa48
Peter Yao <93311390+PeterYaoNYU@users.noreply.github.com>
2023-11-27 21:51:19 +0800

    the selected region can also be sth like Manhatten, add borough option. @Wenbo Please note this change

--- src/regionSelector.js
+++ src/regionSelector.js
@@ -8,11 +8,50 @@ const RegionSelector = ({ neighborhoods, setSelectedRegion, selectedRegion }) =>
 
   console.log(selectedRegion)
 
-  return (
-    <div>
-      <select value={selectedRegion} onChange={handleSelectChange} className="form-select">
+//   return (
+//     <div>
+//       <select value={selectedRegion} onChange={handleSelectChange} className="form-select">
+        // <option value="Manhatten">Manhattan</option>
+        // <option value="Brooklyn">Brooklyn</option>
+        // <option value="Queens">Queens</option>
+        // <option value="Bronx">Bronx</option>
+        // <option value="Staten Island">Staten Island</option>
+
+
+        // {Object.entries(neighborhoods).map(([group, neighborhoods]) => (
+        //   <optgroup key={group} label={group}>
+        //     {/* <option value={group}>{group}</option> */}
+        //     {neighborhoods.map((neighborhood, index) => (
+        //       <option key={index} value={neighborhood}>
+        //         {neighborhood}
+        //       </option>
+        //     ))}
+        //   </optgroup>
+        // ))}
+//       </select>
+//     </div>
+//   );
+
+return (
+    <div className="mb-3">
+      <label htmlFor="neighborhood-select" className="form-label">Filter by:</label>
+      <select 
+        id="neighborhood-select"
+        className="form-select" 
+        value={selectedRegion} 
+        onChange={handleSelectChange}
+        style={{ maxWidth: '300px' }} // Adjust width as needed
+      >
+        <option value="Manhatten">Manhattan</option>
+        <option value="Brooklyn">Brooklyn</option>
+        <option value="Queens">Queens</option>
+        <option value="Bronx">Bronx</option>
+        <option value="Staten Island">Staten Island</option>
+
+
         {Object.entries(neighborhoods).map(([group, neighborhoods]) => (
           <optgroup key={group} label={group}>
+            {/* <option value={group}>{group}</option> */}
             {neighborhoods.map((neighborhood, index) => (
               <option key={index} value={neighborhood}>
                 {neighborhood}
